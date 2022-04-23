fd2024ec398b6bbee5538257b12307c447e109a1
younan nagy <younan.nagy2@gmail.com>
2022-04-23 19:59:17 +0200

    Update Medium.py

--- Medium.py
+++ Medium.py
@@ -302,3 +302,35 @@ class Solution:
             result=result+"b"
             return result
             
+17. Letter Combinations of a Phone Number
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        results=[]
+        compination=""
+        mapping={
+            "2":"abc"
+            ,"3":"def"
+            ,"4":"ghi"
+            ,"5":"jkl"
+            ,"6":"mno"
+            ,"7":"pqrs"
+            ,"8":"tuv"
+            ,"9":"wxyz"
+                
+                }
+        
+        def addResults(digit):
+            combinations=[]
+            if len(results)==0:
+                for char in mapping[digit]:
+                    combinations.append(char)
+                return combinations
+
+            for i in range(0,len(results)):
+                for char in mapping[digit]:
+                    combinations.append(results[i]+char)
+            return combinations
+
+        for digit in digits:
+            results=addResults(digit)
+        return results
