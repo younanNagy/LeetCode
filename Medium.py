864ece293c21edc619eccceeedad37e8d7448d24
younan nagy <younan.nagy2@gmail.com>
2022-05-14 22:26:53 +0200

    Update Medium.py

--- Medium.py
+++ Medium.py
@@ -371,4 +371,32 @@ class Solution:
         head=self.buildLinkedList(numb1+numb2)
         return head 
         
-        
+  
+102. Binary Tree Level Order Traversal
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        que=[]
+        traversed=[]
+        que.append(root)
+        while que:
+            level_length=len(que)
+            level=[]
+            for i in range(0,level_length):
+                node=que.pop(0)
+                level.append(node.val)
+                if node.left:
+                    que.append(node.left)
+                if node.right:
+                    que.append(node.right)
+            if len(level)>0:
+                traversed.append(level)
+                
+        return traversed
